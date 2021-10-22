9ee31c314aae028f61db9f2a835f0cbc3271113a
shift-runstop <shift-runstop@0xFF>
2021-10-22 10:24:09 +0100

    argparse, parsing terminate ec2 --function not working in ec2_control.py

--- README.md
+++
@@ -1,8 +0,0 @@
-# AWS control program
-
-## TODO
-
-- Fix sys.argv parameter bug
-- add finished functions to argparse in main.py
-- set up autoload functions
-- add lambda test 
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ parser=argparse.ArgumentParser()
 # ec2 parsing
 parser.add_argument("-c-ec2","--create-ec2", dest='cec2', help='create an ec2 instance you will be prompted for an ssh key')
 parser.add_argument("-ls-ec2", "--list-ec2", dest='lsec2', help='List all ec2 instances and associated information')
-
+parser.add_argument("-rm-ec2", "--remove-ec2", dest='rmec2', help='Terminate ec2 instance by specifying instance id by passing the id as an argument')
 #s3 parsing
 parser.add_argument("-c-s3","--create-s3", dest='cs3', help='create an s3 bucket, location default is eu-west-1, requires bucket name')
 parser.add_argument("-ls-s3", "--list-s3", dest='ls3k', help='List all s3 buckets')
@@ -18,12 +18,15 @@ args=parser.parse_args()
 def main():
     if args.cec2:
         ec2_control.create_ec2()
+    if args.lsec2:
+        ec2_control.list_instances()
+    if args.rmec2:
+        ec2_control.terminate_ec2()
     if args.cs3:
         s3_control.create_s3()
     if args.ls3k:
         s3_control.list_s3_and_keys()
-    if args.lsec2:
-        ec2_control.list_instances()
+    
 
 if __name__ == "__main__":
     main()
