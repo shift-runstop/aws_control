2d97c698e5cf17f34f2787cf68d2a8d7803b2491
shift-runstop <shift-runstop@0xFF>
2021-10-25 13:51:10 +0100

    added sleep for httpd process recog

--- monitor.sh
+++ monitor.sh
@@ -7,7 +7,9 @@
 # | | | | | | (_) | | | | | || (_) | |   
 # |_| |_| |_|\___/|_| |_|_|\__\___/|_|   
 #
-                                       
+
+sleep 5
+
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 MEMORYUSAGE=$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2 }')
 PROCESSES=$(expr $(ps -A | grep -c .) - 1)
@@ -25,10 +27,10 @@ echo "Memory utilisation: $MEMORYUSAGE"
 echo "No of processes: $PROCESSES"
 
 if [ $HTTPD_PROCESSES -ge 1 ]
-then
-    echo "Web server is running"
-else
-    echo "Web server is NOT running"
+    then
+        echo "Web server is running"
+    else
+        echo "Web server is NOT running"
 fi
 
 echo '<----------------->'
